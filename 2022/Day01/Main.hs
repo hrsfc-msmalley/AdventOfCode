d9a9a1767bfe1871d1f50ea651f75f5b34dbcbfa
hrsfc-msmalley <119405123+hrsfc-msmalley@users.noreply.github.com>
2022-12-03 11:42:04 +0000

    Update Main.hs

--- 2022/Day01/Main.hs
+++ 2022/Day01/Main.hs
@@ -3,14 +3,13 @@ import Data.List
 
 sumGroups :: [String] -> [Int] -> [Int]
 sumGroups [] ns = ns
-sumGroups (x:xs) (n:ns)
-  | x == "" = sumGroups xs (0:n:ns)
-  | otherwise = sumGroups xs ((n + read x):ns)
+sumGroups ("":xs) ns = sumGroups xs (0:ns)
+sumGroups (x:xs) (n:ns) = sumGroups xs ((n + read x):ns)
 
 main = do
   handle <- openFile "input.txt" ReadMode
   contents <- hGetContents handle
-  let outputtop = sumGroups (lines contents) [0]
-  print (take 1 (reverse (sort outputtop)))
-  print (sum (take 3 (reverse (sort outputtop))))
+  let outputtop = sort (sumGroups (lines contents) [0])
+  print (sum (take 1 (reverse (outputtop))))
+  print (sum (take 3 (reverse (outputtop))))
   hClose handle
